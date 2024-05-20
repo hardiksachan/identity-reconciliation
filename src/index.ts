cb8c8b3c1538229e5e4ec6f68eb5fd6bfd09d4ac
hardiksachan <sachanhardik@gmail.com>
2024-05-21 02:41:00 +0530

    setup logging + more ts config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 const port = process.env.PORT || "8000";
 
 const app = express();
+app.use(morgan("dev"));
 
 app.get("/health", (_, res) => {
   res.status(200).send();
